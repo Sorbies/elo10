ec20ee9b6f3634aaca355756fd2898badd78a3ed
Eric Lo <elo10@stuy.edu>
2021-05-05 13:07:14 -0400

    added k26

--- 26_canvas-anim/animate.js
+++ 26_canvas-anim/animate.js
@@ -1,7 +1,7 @@
-// Clyde Sinclair
-// SoftDev pd0
+// Team e -- Ari Schechter, Eric Lo, William Yin
+// SoftDev pd1
 // K26 -- canvas based JS animation
-// 2021-05-05w
+// 2021-05-05
 
 // model for HTML5 canvas-based animation
 
@@ -19,38 +19,32 @@ var ctx = c.getContext("2d");// YOUR CODE HERE
 //set fill color to team color
 ctx.fillStyle = "red" // YOUR CODE HERE
 
-var requestID;  //init global var for use with animation frames
+var requestID = 0;  //init global var for use with animation frames
 
 
 //var clear = function(e) {
 var clear = (e) => {
-    console.log("clear invoked...")
-    ctx.clearRect(0, 0, c.width, c.height);
     // YOUR CODE HERE
+    ctx.clearRect(0, 0, c.width, c.height);
 };
 
 
 var radius = 0;
 var growing = true;
-var start;
 
 
 //var drawDot = function() {
 var drawDot = () => {
-    requestID = window.requestAnimationFrame(animateCircle);
+    if (requestID === 0) requestID = animateCircle();
 };
 
-var animateCircle = (timestamp) => {
-    console.log("drawDot invoked...")
-    if (start === undefined)
-        start = timestamp;
-    const elapsed = timestamp - start;
+var animateCircle = () => {
     // YOUR CODE HERE
     clear()
     ctx.beginPath();
 
     // arc draws the circle shape, but cannot immediatly be seen on the canvas
-    ctx.arc(250, 250, radius, 0, (2 * Math.PI));
+    ctx.arc(c.width / 2, c.height / 2, radius, 0, (2 * Math.PI));
 
     // stroke adds an outline to the circle shape so that it can be seen
     ctx.stroke();
@@ -58,34 +52,25 @@ var animateCircle = (timestamp) => {
     // fill fills in the circle, finishing our shape
     ctx.fill();
 
-    if (growing) {radius++;}
-    else {radius--;}
+    if (radius >= 250) {
+        growing = false
+    } else if (radius <= 0) {
+        growing = true;
+    }
+    if (growing) radius++;
+    else radius--;
+
     requestID = window.requestAnimationFrame(animateCircle);
-    if (radius == 250) {growing = false;}
-    /*
-      ...to
-      Wipe the canvas,
-      Repaint the circle,
-      ...and somewhere (where/when is the right time?)
-      Update requestID to propagate the animation.
-      You will need
-      window.cancelAnimationFrame()
-      window.requestAnimationFrame()
-     */
+    return requestID;
 };
 
 //var stopIt = function() {
 var stopIt = () => {
-    console.log("stopIt invoked...")
-    console.log(requestID);
-
     // YOUR CODE HERE
-    /*
-      ...to
-      Stop the animation
-      You will need
-      window.cancelAnimationFrame()
-    */
+    if (requestID !== 0) {
+        window.cancelAnimationFrame(requestID);
+        requestID = 0;
+    }
 };
 
 dotButton.addEventListener("click", drawDot);
